5466b87dd14f2b7c1542a875169c0f90627ccf2f
srnyx <25808801+srnyx@users.noreply.github.com>
2024-07-03 21:32:22 -0400

    Update dependencies & bump version (1.0.1)
    Gradle Galaxy: 1.1.3 -> 1.2.2
    Shadow: 8.1.7 -> 8.1.8
    Annoying API: 274e18ac01 -> 5.0.0
    Gradle: 8.7 -> 8.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.3"
-    id("io.github.goooler.shadow") version "8.1.7"
+    id("xyz.srnyx.gradle-galaxy") version "1.2.2"
+    id("io.github.goooler.shadow") version "8.1.8"
 }
 
-setupAnnoyingAPI("274e18ac01", "xyz.srnyx", "1.0.0", "Adds a customizable way to craft vehicles for the Vehicles Spigot plugin")
 spigotAPI("1.15")
+setupAnnoyingAPI("5.0.0", "xyz.srnyx", "1.0.1", "Adds a customizable way to craft vehicles for the Vehicles Spigot plugin")
+
 dependencies.compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar")))) // Vehicles
